7afb7f888f103b6a1353cceb911cd37cf8eb8a57
InduRani11 <pavitramindu999@gmail.com>
2025-03-27 21:22:29 +0530

    test 1 dn

--- Day14-23march/app.js
+++ Day14-23march/app.js
@@ -1,23 +1,59 @@
 
-var ax=10;
-var ay=50;
-abc();
+// let ax=10;
+// var ay=50;
+// abc();
+//
+// function abc(){
+//     var g=55;
+//     var h=40;
+//
+//     function x(){
+//         console.log("X executed")
+//         const arrw=()=>{
+//             console.log("arrow fn");
+//         }
+//         arrw();
+//     }
+//     function y(){
+//         console.log("Y executed")
+//     }
+//     x();
+//     y();
+//
+// }
 
-function abc(){
-    var g=55;
-    var h=40;
+// hoisting
 
-    function x(){
-        console.log("X executed")
-        const arrw=()=>{
-            console.log("arrow fn");
-        }
-        arrw();
+
+// //closures
+// function getCreateCounter(){
+//     var x=3;
+//     var y=7;
+//     return function createCounter(){
+//         var curr=0;
+//         function count(){
+//             curr++;
+//             console.log(curr,x);
+//         }
+//         return count;
+//     }
+//
+// const createCounter=getCreateCounter();
+// const cont=createCounter();
+// cont();
+// cont();
+// cont();
+// cont();
+// cont();
+// var x=10;
+// function executeThis(){
+//     console.log("1234",10)
+// }
+// setTimeout(executeThis,5000)
+function getFun(){
+    var x=10;
+    return function executeThis(){
+        console.log("1234",10)
     }
-    function y(){
-        console.log("Y executed")
-    }
-    x();
-    y();
-
 }
+setTimeout(getFun(),5000)
---
+++ Test 1/app.js
@@ -0,0 +1,49 @@
+const main=document.getElementById("main");
+const input=document.getElementById('input');
+const apiUsers='https://jsonplaceholder.typicode.com/users';
+const imageApi='https://robohash.org/'
+fetch(apiUsers).then((data)=>{
+    console.log(data)
+    return data.json();
+}).then((data)=>{
+    console.log(data);
+        // let data1=data.filter(val,()=>{ if(val.name.contain(input.value)) return val})
+        for(let i=0;i<data.length;i++){
+            createADiv(data,i);
+        }
+        input.addEventListener('keyup',()=>{
+            const divs= document.querySelectorAll('.card');
+            for(let i=0;i<divs.length;i++){
+                main.removeChild(divs[i]);
+            }
+            // main.removeChild(document.querySelector('.card'))
+            if(input.value==''){
+                for(let i=0;i<data.length;i++){
+                    createADiv(data,i);
+                }
+            }else{
+                let data1=data.filter((val,i)=>{ return val.name.includes(input.value) ||val.name.includes(input.value.toUpperCase())||val.name.includes(input.value.toLowerCase())})
+                console.log(data1);
+                for(let i=0;i<data1.length;i++){
+                    createADiv(data1,i);
+                }
+            }
+        });
+});
+function createADiv(data,index){
+    let div=document.createElement('div');
+    div.classList.add("card");
+    let img=document.createElement('img');
+    img.setAttribute('src',`${imageApi}${index}`)
+    let h3=document.createElement('h2');
+    h3.innerText=data[index].name;
+    console.log()
+    let p1=document.createElement('p');
+    p1.classList.add("email-para");
+    p1.innerText=data[index].email
+    let p2=document.createElement('p');
+    p1.classList.add("username-para");
+    p2.innerText=data[index].username;
+    div.append(img,h3,p1,p2);
+    main.append(div);
+}
